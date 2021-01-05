04cdf057e8960590f19c95b1410cd21498f17f49
firozkabirroz <firozkabirroz@gmail.com>
2021-01-06 00:18:04 +0600

    chapter 8 edited

--- chapter8.py
+++ chapter8.py
@@ -47,11 +47,15 @@ def getContours(img):
             x, y, w, h = cv2.boundingRect(approx)
 
             if objectCorner == 3: objectType = "Tri"
+            elif objectCorner ==4:
+                aspRatio = w/float(h)
+                if aspRatio > 0.95 and aspRatio < 1.05: objectType = "Square"
+                else:objectType="Rectangle"
             else:objectType="None"
 
             cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),2)
             cv2.putText(imgContour,objectType,
-                        (x+(w//2)-10,y+(y//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.7,
+                        (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.7,
                         (0,0,0),2)
 
 
@@ -72,7 +76,7 @@ imgCanny = cv2.Canny(imgBlur,50,50)
 getContours(imgCanny)
 
 imgBlank = np.zeros_like(img)
-imgStack = stackImages(0.8,([img,imgGray,imgBlur],
+imgStack = stackImages(0.6,([img,imgGray,imgBlur],
                             [imgCanny,imgContour,imgBlank]))
 
 
